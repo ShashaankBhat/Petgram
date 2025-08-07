57b5623fbc2170a8f2a5ad6af2139669a4f67e6c
Shashank Bhat <yashbhat2004@gmail.com>
2025-08-07 12:23:25 +0530

    Initial commit – Petgram setup

---
+++ frontend/src/components/svgs/X.jsx
@@ -0,0 +1,6 @@
+import { BiBorderRadius } from "react-icons/bi";
+
+const XSvg = (props) => (
+	<img src="/favicon.ico" alt="" {...props} style={{borderRadius: "50%"}} />
+);
+export default XSvg;
---
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import XSvg from "../../../components/svgs/X";
+
+import { MdOutlineMail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const LoginPage = () => {
+	const [formData, setFormData] = useState({
+		username: "",
+		password: "",
+	});
+	const queryClient = useQueryClient();
+
+	const {
+		mutate: loginMutation,
+		isPending,
+		isError,
+		error,
+	} = useMutation({
+		mutationFn: async ({ username, password }) => {
+			try {
+				const res = await fetch("/api/auth/login", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ username, password }),
+				});
+
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			// refetch the authUser
+			queryClient.invalidateQueries({ queryKey: ["authUser"] });
+		},
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		loginMutation(formData);
+	};
+
+	const handleInputChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	return (
+		<div className='mx-auto flex h-screen'>
+			<div className="parth-bg"></div>
+			<div className='flex-1 flex flex-col justify-center items-center'>
+				<form className='flex gap-4 flex-col form' onSubmit={handleSubmit}>
+					<h1 className='text-4xl font-extrabold text-white'>Welcome to PetGram</h1>
+					<label className='input input-bordered rounded flex items-center gap-2'>
+						<MdOutlineMail />
+						<input
+							type='text'
+							className='grow'
+							placeholder='username'
+							name='username'
+							onChange={handleInputChange}
+							value={formData.username}
+						/>
+					</label>
+
+					<label className='input input-bordered rounded flex items-center gap-2'>
+						<MdPassword />
+						<input
+							type='password'
+							className='grow'
+							placeholder='Password'
+							name='password'
+							onChange={handleInputChange}
+							value={formData.password}
+						/>
+					</label>
+					<button className='btn rounded-full btn-primary text-white'>
+						{isPending ? "Loading..." : "Login"}
+					</button>
+					{isError && <p className='text-red-500'>{error.message}</p>}
+
+					<div className='flex flex-col gap-2 mt-4'>
+					<p className='text-white text-lg' style={{textAlign: "center"}}>{"Don't"} have an account?</p>
+					<Link to='/signup'>
+						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
+					</Link>
+				</div>
+				</form>
+				
+			</div>
+		</div>
+	);
+};
+export default LoginPage;
